920fd710e0e46bb0e76d3f6405e402fb009e0d4f
SofianAcDev <sofian.achbabi@gmail.com>
2023-04-02 11:33:47 +0200

    meal-app

---
+++ app.js
@@ -0,0 +1,32 @@
+const result = document.getElementById("result");
+const form = document.querySelector("form");
+let meals = [];
+
+async function fetchMeals() {
+  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    .then((res) => res.json())
+    .then((data) => (meals = data.meals));
+
+  console.log(meals);
+}
+
+function mealsDisplay() {
+  meals.length = 12;
+
+  result.innerHTML = meals.map(
+    (meal) =>
+      `
+      <li class="card">
+    <h2>${meal.strMeal}</h2>
+    <p>${meal.strArea}</p>
+    <img src="${meal.strMealThumb}" alt="photo de ${meal.strMeal}/>
+    <ul></ul>
+    </li>
+    `
+  );
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchMeals().then(() => mealsDisplay());
+});
