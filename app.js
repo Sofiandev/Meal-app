a86628f6a1c7b01086844f245402b422ec07618f
SofianAcDev <sofian.achbabi@gmail.com>
2023-04-04 17:05:59 +0200

    + ingredients & measure

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const result = document.getElementById("result");
 const form = document.querySelector("form");
+const input = document.querySelector("input");
 let meals = [];
 
-async function fetchMeals() {
-  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+async function fetchMeals(search) {
+  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
     .then((res) => res.json())
     .then((data) => (meals = data.meals));
-
-  console.log(meals);
 }
 
 function mealsDisplay() {
-  meals.length = 12;
+  if (meals === null) {
+    result.innerHTML = "<h2> Aucun Résultat</h2>";
+  } else {
+    meals.length = 12;
 
-  result.innerHTML = meals.map(
-    (meal) =>
-      `
-      <li class="card">
-    <h2>${meal.strMeal}</h2>
-    <p>${meal.strArea}</p>
-    <img src="${meal.strMealThumb}" alt="photo de ${meal.strMeal}/>
-    <ul></ul>
+    result.innerHTML = meals
+      .map((meal) => {
+        let ingredients = [];
+
+        for (i = 1; i < 21; i++) {
+          if (meal[`strIngredient${i}`]) {
+            let ingredient = meal[`strIngredient${i}`];
+            let measure = meal[`strMeasure${i}`];
+
+            ingredients.push(`<li>${ingredient} - ${measure}</li>`);
+          }
+        }
+
+        return `
+    <li class="card">
+        <h2>${meal.strMeal}</h2>
+        <p>${meal.strArea}</p>
+        <img src="${meal.strMealThumb}" alt="photo de ${meal.strMeal}"/>
+        <ul>${ingredients.join("")}</ul>
     </li>
-    `
-  );
+    `;
+      })
+      .join("");
+  }
 }
+input.addEventListener("input", (e) => {
+  fetchMeals(e.target.value).then(() => mealsDisplay());
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  fetchMeals().then(() => mealsDisplay());
+  mealsDisplay();
 });
